3a88a3641755ca2542d35daeba2bd777ec795408
laidbackpeter <ndegwa.muchina3@gmail.com>
2017-05-10 12:23:51 +0300

    Index page rendering has been added

--- web_app/views.py
+++ web_app/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'web_app/index.html', context=None)
